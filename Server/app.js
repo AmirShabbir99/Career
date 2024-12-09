520929dba3fac4c4954612e17d1540423d128ad9
M.Amir Shabbir <m.amirshabbir99@gmail.com>
2024-12-09 15:04:17 +0500

    Work done

---
+++ Server/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import userRouter from "./routes/userRouter.js";
+import jobRouter from "./routes/jobRouter.js";
+import applicationRouter from "./routes/applicationRouter.js";
+import dbConnection from "./database/dbconnection.js";
+
+const app = express();
+dotenv.config();
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/temp/",
+  })
+);
+
+app.use("/api/user", userRouter);
+app.use("/api/job", jobRouter);
+app.use("/api/application", applicationRouter);
+dbConnection();
+
+export default app;
