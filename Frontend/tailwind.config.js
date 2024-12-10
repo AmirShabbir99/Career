3c76246af2db24f267642988213f674b28dfd0bf
M.Amir Shabbir <m.amirshabbir99@gmail.com>
2024-12-10 08:53:54 +0500

    T-V deploy

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,7 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+export default {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "hero-gradient": "linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
