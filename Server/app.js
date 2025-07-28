2e698baa552385487ff21670dace9678d424746f
M.Amir Shabbir <m.amirshabbir99@gmail.com>
2025-07-28 16:03:21 +0500

    cors set

--- Server/app.js
+++ Server/app.js
@@ -12,7 +12,7 @@ const app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
     methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
